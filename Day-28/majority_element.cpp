7e8ad9be034c0e2a33044a91cbc07bd46f938a18
Rahul <304365229+chaudharirahul10@users.noreply.github.com>
2026-09-09 21:04:11 +0530

    finding majority element from the given array

---
+++ Day-28/majority_element.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {1, 1, 2, 2, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int majorityElement = arr[0];
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        if(count == 0){
+            majorityElement = arr[i];
+        }
+        if(arr[i] == majorityElement){
+            count++;
+        }
+        else{
+            count--;
+        }
+    }
+    count = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] == majorityElement){
+            count++;
+        }
+    }
+    if(count > n / 2){
+        cout << "The majority element is: "
+             << majorityElement << endl;
+    }
+    else{
+        cout << "No majority element found." << endl;
+    }
+    return 0;
+}
